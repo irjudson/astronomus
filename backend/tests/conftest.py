7f642cf5f0374b6b0158c029f7be1f97b4ac8362
Astro Planner <planner@astro.local>
2026-05-27 08:32:07 -0600

    style: fix isort ordering in tests/conftest.py

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -125,9 +125,8 @@ def setup_test_db_schema():
     """
     import os
 
-    from alembic.config import Config
-
     from alembic import command
+    from alembic.config import Config
     from app.core.config import get_settings
 
     settings = get_settings()
